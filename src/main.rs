6bb6891d7f9fb313f5f9068d6b4d7b1c9b93a7ed
Salvador Guzman <guzmansalv@gmail.com>
2018-06-29 12:33:14 -0700

    print articles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate reqwest;
+extern crate select;
+
+static URL: &str = "http://23.95.221.108/page/2";
+
 fn main() {
-    println!("Hello, world!");
+    let body = reqwest::get(URL).unwrap().text().unwrap();
+    let doc = select::document::Document::from(body.as_str());
+    let sel = doc.find(select::predicate::Name("article"));
+
+    for node in sel {
+        let id = node.attr("id").unwrap();
+        println!("{} ({})", node.name().unwrap(), id);
+    }
 }
